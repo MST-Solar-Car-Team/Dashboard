a976378c312103f2ff22041f032756eee0022926
SirMuuffin <ilfhsup@gmail.com>
2025-05-10 22:34:31 -0500

    basic serial implementation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 use slint::{ComponentHandle, SharedString};
+use std::collections::VecDeque;
 
 
 
@@ -12,10 +13,25 @@ slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let window = Dashboard::new()?; // From the Slint DSL
+    // println!("fuck you");
+    // Find the first available serial port and use it
+    let port_info = serialport::available_ports()?
+        .into_iter()
+        .next()
+        .expect("No serial ports found");
+    let mut ports = serialport::new(port_info.port_name, 115200)
+        .timeout(Duration::from_millis(50))
+        .open()
+        .expect("Failed to open serial port!");
+
+    // Circular buff implmentation would be nice at some point
+    let mut queue: VecDeque<u8> = VecDeque::new();
+    let mut serial_buf: Vec<u8> = vec![0; 32];
 
     let ui_handle = window.as_weak();
     thread::spawn(move || {
         let mut speed = 0;
+        let mut throttle = 0;
         loop {
             // Increment speed by 1 every second
             speed = (speed + 1) % 121;
@@ -23,10 +39,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let left_on = false;
             let right_on = true;
             let voltage = 52.3;
-            let throttle = 75;
+            // let mut throttle: u16;
             let temp_bms = 32;
             let temp_motor = 45;
             let temp_controller = 38;
+
+            
+
+            // println!("loops");
+            match ports.read(serial_buf.as_mut_slice()) {
+                Ok(t) => {
+                    for item in &serial_buf[..t] {
+                        queue.push_back(item.to_owned());
+                    }
+                }
+                Err(e) => print!(""),
+            }
+
+            while queue.len() > 16 {
+                let packet_byte = queue.pop_front();
+
+                if let Some(packet_id) = packet_byte {
+                    if packet_id == 0x13 {
+                        let packet_full = queue.make_contiguous();
+                        let packet_full = &packet_full[..15];
+                        // read_pedal_packet(&packet_full[..15]);
+                        throttle = ((packet_full[2] as u16) << 8) | (packet_full[3] as u16);
+                        println!("throttle: {}", throttle);
+                        throttle = (1023-throttle) * 100 / 1023;
+                        println!("new throttle: {}", throttle);
+                        for i in 0..15 {
+                            queue.pop_front();
+                        }
+                    } else {
+                        println!("Packet tossed yo")
+                    }
+                }
+            }
 
             // Update UI on the event loop
             let _ = ui_handle.upgrade_in_event_loop(move |window| {
@@ -34,16 +83,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 window.set_leftBlinkerOn(left_on);
                 window.set_rightBlinkerOn(right_on);
                 window.set_voltage(voltage);
-                window.set_throttle(throttle);
+                window.set_throttle(throttle as i32);
                 window.set_tempBMS(temp_bms);
                 window.set_tempMotor(temp_motor);
                 window.set_tempController(temp_controller);
             });
-
-            thread::sleep(Duration::from_secs(1));
+            // thread::sleep(Duration::from_secs(1));
         }
     });
 
     window.run();
     Ok(())
+}
+
+fn read_pedal_packet(data: &[u8]) {
+    let baseline: u16 = ((data[0] as u16) << 8) | (data[1] as u16);
+    let pedal: u16 = ((data[2] as u16) << 8) | (data[3] as u16);
+
+    println!("baseline: {} pedal: {}", baseline, pedal);
 }
