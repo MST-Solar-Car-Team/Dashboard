1115ef0448198a84e645eb974af940caa44dee57
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-06-25 03:59:45 -0500

    my nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  packages = [ pkgs.gdb ];
-  buildInputs = with pkgs;[ stdenv rustc cargo udev pkg-config libxkbcommon ];
+  packages = [ pkgs.gdb pkgs.opencv pkgs.llvmPackages.libclang.lib];
+  buildInputs = with pkgs;[ stdenv rustc cargo udev pkg-config libxkbcommon rust-analyzer clang llvm llvmPackages.libclang lld opencv];
 
-  # { pkgs ? import <nixpkgs> {} }:  
+
+  
   
 # pkgs.mkShell {  
   # buildInputs = with pkgs; [  
@@ -30,5 +31,11 @@ pkgs.mkShell {
     # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXi}/lib:${pkgs.libxkbcommon}/lib
     export WINIT_UNIX_BACKEND=X11
+    # export RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    #export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+    export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
+
     '';
+
 }
+
