f7d7120d25997117eaa14caf10738dcc4e4bb2e6
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-06-27 01:01:26 -0500

    split serial and window updates into two threads

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   packages = [ pkgs.gdb ];
-  buildInputs = with pkgs;[ stdenv rustc cargo udev pkg-config libxkbcommon ];
+  buildInputs = with pkgs;[ stdenv rustc cargo rust.packages.stable.rustPlatform.rustLibSrc  udev pkg-config libxkbcommon ];
 
   # { pkgs ? import <nixpkgs> {} }:  
   
@@ -30,5 +30,6 @@ pkgs.mkShell {
     # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXi}/lib:${pkgs.libxkbcommon}/lib
     export WINIT_UNIX_BACKEND=X11
+    export RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     '';
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use serialport::{Error, SerialPort};
 use slint::{ComponentHandle, SharedString, ToSharedString};
 use std::collections::VecDeque;
 use std::io::ErrorKind;
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 slint::include_modules!();
@@ -20,7 +22,83 @@ const PEDAL_PACKET_ID: u8 = 0x04;
 const MOTOR_TEMPATURE_PACKET_ID: u8 = 0x05;
 const MOTOR_STATUS_PACKET_ID: u8 = 0x06;
 
-fn make_connection() -> Box<dyn SerialPort> {
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let window = Dashboard::new()?; // From the Slint DSL 
+    let ui_handle = window.as_weak();
+    //initialize default values for window
+    let _ = ui_handle.upgrade_in_event_loop(move |window| {
+        window.set_speed(0.0 as f32);
+        window.set_leftBlinkerOn(false);
+        window.set_rightBlinkerOn(false);
+        window.set_throttle(0 as i32);
+        window.set_tempBMS(0 as i32);
+        window.set_tempMotor(0.0 as f32);
+        window.set_headlightsOn(false);
+        window.set_limitId(0 as i32);
+        window.set_errorOut("".to_shared_string());
+        window.set_serialConnection(false);
+    });
+
+    let mut data_arc_window: Arc<Mutex<WindowData>> = Arc::new(Mutex::new(WindowData::default()));
+    let mut data_arc_serial: Arc<Mutex<WindowData>> = Arc::clone(&data_arc_window);
+
+    thread::spawn(move || {
+        loop {
+            let data = data_arc_window.lock();
+
+            if data.is_err() {
+                continue;
+            }
+            let data = data.unwrap().clone();
+
+
+            // Update UI on the event loop
+            let _ = ui_handle.upgrade_in_event_loop(move |window| {
+                window.set_speed(data.speed as f32);
+                window.set_leftBlinkerOn(data.left_on);
+                window.set_rightBlinkerOn(data.right_on);
+                window.set_throttle(data.throttle as i32);
+                window.set_tempBMS(data.temp_bms as i32);
+                window.set_tempMotor(data.temp_motor);
+                window.set_headlightsOn(data.headlights_on);
+                window.set_limitId(data.limit_code as i32);
+                window.set_errorOut(data.error_out);
+                window.set_serialConnection(data.has_serial_connection);
+            });
+
+            //don't overload the screen
+            thread::sleep(Duration::from_millis(50));
+
+        }
+    });
+
+    thread::spawn(move || {
+        update_serial(data_arc_serial);
+    });
+
+    _ = window.run();
+
+
+    Ok(())
+}
+
+
+#[derive(Clone, Debug, Default)]
+struct WindowData {
+    speed: f32, 
+    throttle: i32, 
+    headlights_on: bool, 
+    left_on: bool, 
+    right_on: bool,
+    temp_bms: f32,
+    temp_motor: f32,
+    limit_code: u16 ,
+    error_out: SharedString,
+    has_serial_connection: bool, 
+}
+
+fn make_connection() -> Box<dyn SerialPort + 'static> {
     loop {
         let available_ports = serialport::available_ports().unwrap().into_iter();
         
@@ -50,164 +128,128 @@ fn make_connection() -> Box<dyn SerialPort> {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let window = Dashboard::new()?; // From the Slint DSL 
-    let ui_handle = window.as_weak();
-    //initialize default values for window
-    let _ = ui_handle.upgrade_in_event_loop(move |window| {
-        window.set_speed(0.0 as f32);
-        window.set_leftBlinkerOn(false);
-        window.set_rightBlinkerOn(false);
-        window.set_throttle(0 as i32);
-        window.set_tempBMS(0 as i32);
-        window.set_tempMotor(0.0 as f32);
-        window.set_headlightsOn(false);
-        window.set_limitId(0 as i32);
-        window.set_errorOut("".to_shared_string());
-        window.set_serialConnection(false);
-    });
+fn update_serial(data:Arc<Mutex<WindowData>>) { // port:Box<dyn SerialPort + 'static>) -> Result<(),()>{
+    let mut port = make_connection();
+    // Circular buff implmentation would be nice at some point
+    let mut queue: VecDeque<u8> = VecDeque::new();
+    let mut serial_buf: Vec<u8> = vec![0; 32];
 
-
-
-
-    thread::spawn(move || {
-        let mut port = make_connection();
-        // Circular buff implmentation would be nice at some point
-        let mut queue: VecDeque<u8> = VecDeque::new();
-        let mut serial_buf: Vec<u8> = vec![0; 32];
-
-
-
-        let mut speed = 0.0;
-        let mut throttle = 0;
-        let mut headlights_on = false;
-        let mut left_on = false;
-        let mut right_on = false;
-        let mut temp_motor = 0.0;
-        let mut limit_code: u16 = 0;
-        let mut error_out: SharedString = "".to_shared_string();
-        let mut has_serial_connection = true;
-
-        loop {
-            let temp_bms = 0;
-            //let mut error_out: SharedString = "".to_shared_string();
-            
-            match port.read(serial_buf.as_mut_slice()) {
-                Ok(t) => {
-                    println!("Buf:{:?}",serial_buf);
-                    has_serial_connection = true;
-                    for item in &serial_buf[..t] {
-                        queue.push_back(item.to_owned());
-                    }
+    loop {
+        
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+                println!("Buf:{:?}",serial_buf);
+                //updates serial working status
+                if let Ok(mut mutex) = data.lock(){
+                    mutex.has_serial_connection = true;
                 }
-                Err(e) => {
-                    if e.kind() == ErrorKind::BrokenPipe {
-                        port = make_connection();
-                        println!("Recovered port!");
-                    }
-                    if e.kind() == ErrorKind::TimedOut{
-                        has_serial_connection = false;
-                    }
-                    println!("Random Error: {}", e);
+                
+                for item in &serial_buf[..t] {
+                    queue.push_back(item.to_owned());
                 }
             }
-
-            while queue.len() > 16 {
-                let packet_byte = queue.pop_front();
-
-                if let Some(packet_id) = packet_byte {
-                    match packet_id {
-                        PEDAL_PACKET_ID => {
-                            if let Ok(pedal_packet) = PedalPacket::from_bytes(&[
-                                queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
-                                queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
-                                queue[12], queue[13], queue[14],
-                            ]) {
-                                throttle = pedal_packet.get_throttle_percentage();
-
-                                if (pedal_packet.baseline_value
-                                    < (pedal_packet.pedal_value.clamp(50, 1023) - 50))
-                                    || (pedal_packet.pedal_value < 475)
-                                {
-                                    error_out =
-                                        "Warning: Recent pedal fault detected!".to_shared_string();
-                                }
-
-                                queue.drain(0..15);
-                            }
-                        }
-                        VELOCITY_PACKET_ID => {
-                            if let Ok(velocity_packet) = VelocityPacket::from_bytes(&[
-                                queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
-                                queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
-                                queue[12], queue[13], queue[14],
-                            ]) {
-                                speed = velocity_packet.to_mph().trunc();
-
-                                queue.drain(0..15);
-                            }
-                        }
-                        LIGHTS_PACKET_ID => {
-                            if let Ok(lights_packet) = LightsPacket::from_bytes(&[
-                                queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
-                                queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
-                                queue[12], queue[13], queue[14],
-                            ]) {
-                                left_on = lights_packet.left_blinkers;
-                                right_on = lights_packet.right_blinkers;
-                                headlights_on = lights_packet.headlights;
-
-                                queue.drain(0..15);
-                            }
-                        }
-                        MOTOR_TEMPATURE_PACKET_ID => {
-                            if let Ok(motor_temp_packet) = MotorTempaturePacket::from_bytes(&[
-                                queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
-                                queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
-                                queue[12], queue[13], queue[14],
-                            ]) {
-                                temp_motor = motor_temp_packet.motor_temp;
-                            }
-                        }
-                        MOTOR_STATUS_PACKET_ID => {
-                            if let Ok(motor_status_packet) = MotorStatusPacket::from_bytes(&[
-                                queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
-                                queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
-                                queue[12], queue[13], queue[14],
-                            ]) {
-                                limit_code = motor_status_packet.limit_flags;
-                                if motor_status_packet.error_flags != 0 {
-                                    error_out = format!(
-                                        "Warning: Error code {}",
-                                        motor_status_packet.error_flags
-                                    )
-                                    .to_shared_string();
-                                }
-                            }
-                        }
-                        _ => print!(""), //println!("Packet tossed yo: {:#04X?}", packet_id),
-                    }
+            Err(e) => {
+                //tells gui that the serial is broke
+                if let Ok(mut mutex) = data.lock(){
+                    mutex.has_serial_connection = false;
                 }
+
+                if e.kind() == ErrorKind::BrokenPipe {
+                    port = make_connection();
+                    println!("Recovered port!");
+                }
+                if e.kind() == ErrorKind::TimedOut{
+
+                }
+                println!("Random Error: {}", e);
             }
-
-            let warning = error_out.clone();
-
-            // Update UI on the event loop
-            let _ = ui_handle.upgrade_in_event_loop(move |window| {
-                window.set_speed(speed as f32);
-                window.set_leftBlinkerOn(left_on);
-                window.set_rightBlinkerOn(right_on);
-                window.set_throttle(throttle as i32);
-                window.set_tempBMS(temp_bms);
-                window.set_tempMotor(temp_motor);
-                window.set_headlightsOn(headlights_on);
-                window.set_limitId(limit_code as i32);
-                window.set_errorOut(warning);
-                window.set_serialConnection(has_serial_connection);
-            });
         }
-    });
 
-    _ = window.run();
-    Ok(())
+        while queue.len() > 16 {
+            let packet_byte = queue.pop_front();
+
+
+            if packet_byte.is_none(){
+                break; //empty queue
+            }
+            
+            let packet_id = packet_byte.unwrap();
+                
+            if let Ok(mut data) = data.lock(){
+                match packet_id {
+                    PEDAL_PACKET_ID => {
+                        if let Ok(pedal_packet) = PedalPacket::from_bytes(&[
+                            queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
+                            queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
+                            queue[12], queue[13], queue[14],
+                        ]) {
+                            data.throttle = pedal_packet.get_throttle_percentage() as i32;
+
+                            if (pedal_packet.baseline_value
+                                < (pedal_packet.pedal_value.clamp(50, 1023) - 50))
+                                || (pedal_packet.pedal_value < 475)
+                            {
+                                data.error_out =
+                                    "Warning: Recent pedal fault detected!".to_shared_string();
+                            }
+
+                            queue.drain(0..15);
+                        }
+                    }
+                    VELOCITY_PACKET_ID => {
+                        if let Ok(velocity_packet) = VelocityPacket::from_bytes(&[
+                            queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
+                            queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
+                            queue[12], queue[13], queue[14],
+                        ]) {
+                            data.speed = velocity_packet.to_mph().trunc();
+
+                            queue.drain(0..15);
+                        }
+                    }
+                    LIGHTS_PACKET_ID => {
+                        if let Ok(lights_packet) = LightsPacket::from_bytes(&[
+                            queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
+                            queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
+                            queue[12], queue[13], queue[14],
+                        ]) {
+                            data.left_on = lights_packet.left_blinkers;
+                            data.right_on = lights_packet.right_blinkers;
+                            data.headlights_on = lights_packet.headlights;
+
+                            queue.drain(0..15);
+                        }
+                    }
+                    MOTOR_TEMPATURE_PACKET_ID => {
+                        if let Ok(motor_temp_packet) = MotorTempaturePacket::from_bytes(&[
+                            queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
+                            queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
+                            queue[12], queue[13], queue[14],
+                        ]) {
+                            data.temp_motor = motor_temp_packet.motor_temp;
+                        }
+                    }
+                    MOTOR_STATUS_PACKET_ID => {
+                        if let Ok(motor_status_packet) = MotorStatusPacket::from_bytes(&[
+                            queue[0], queue[1], queue[2], queue[3], queue[4], queue[5],
+                            queue[6], queue[7], queue[8], queue[9], queue[10], queue[11],
+                            queue[12], queue[13], queue[14],
+                        ]) {
+                            data.limit_code = motor_status_packet.limit_flags.clone();
+                            if motor_status_packet.error_flags != 0 {
+                                data.error_out = format!(
+                                    "Warning: Error code {}",
+                                    motor_status_packet.error_flags
+                                )
+                                .to_shared_string();
+                            }
+                        }
+                    }
+                    _ => print!(""), //println!("Packet tossed yo: {:#04X?}", packet_id),
+                }
+            }  
+        }
+
+    }
+
 }
